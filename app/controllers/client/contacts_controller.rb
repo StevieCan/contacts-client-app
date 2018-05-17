a1517265c54d66beee8f09264cae9091876b9c04
Steve Han <shan61489@gmail.com>
2018-05-16 21:24:38 -0500

    added full name and friendly reader methods

---
+++ app/controllers/client/contacts_controller.rb
@@ -0,0 +1,60 @@
+class Client::ContactsController < ApplicationController
+  def index
+    response = Unirest.get(
+                          "http://localhost:3000/api/contacts")
+    @contacts = response.body
+    render 'index.html.erb'
+  end
+  def new
+    render 'new.html.erb'
+  end
+  def create
+    client_params = {
+                       id: params[:id],
+                       first_name: params[:first_name],
+                       middle_name: params[:middle_name],
+                       last_name: params[:last_name],
+                       email: params[:email],
+                       phone_number: params[:phone_number],
+                       bio: params[:bio]
+    }
+    response = Unirest.post(
+                            "http://localhost:3000/api/contacts",
+                            parameters: client_params
+                            )
+    render 'create.html.erb'
+  end
+  def show
+    contact_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/contacts/#{contact_id}")
+    @contact = response.body
+    render 'show.html.erb'
+  end
+  def edit
+    @contact_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/contacts/#{@contact_id}")
+    @contact = response.body
+    render 'edit.html.erb'
+  end
+  def update
+    client_params = {
+                     id: params[:id],
+                     first_name: params[:first_name],
+                     middle_name: params[:middle_name],
+                     last_name: params[:last_name],
+                     email: params[:email],
+                     phone_number: params[:phone_number],
+                     bio: params[:bio],
+}
+  response = Unirest.patch(
+                           "http://localhost:3000/api/contacts/#{contact_id}",
+                           parameters: client_params)
+  @contact.save
+  render 'show.html.erb'
+  end
+  def destroy
+    contact_id = params[:id]
+    response = Unirest.delete("http://localhost:3000/api/contacts/#{contact[:id]}")
+    render 'destroy.html.erb'
+  end
+end
